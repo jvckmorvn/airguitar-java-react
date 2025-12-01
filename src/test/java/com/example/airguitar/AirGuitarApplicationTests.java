cfde59ea26cecf35b41132a8d819e62eb9cb1f8c
Jack Moran <jvckmorvn@gmail.com>
2025-12-01 21:56:01 +0000

    init commit

---
+++ src/test/java/com/example/airguitar/AirGuitarApplicationTests.java
@@ -0,0 +1,13 @@
+package com.example.airguitar;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class AirGuitarApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
